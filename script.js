0603375228a235f26686e0f8487a6df6b5a58936
kokubu-yumiko <kokubu-yumiko@kayac.com>
2019-12-13 06:22:17 +0900

    ベース完成

---
+++ script.js
@@ -0,0 +1,66 @@
+const clock = new THREE.Clock();
+const scene = new THREE.Scene();
+const loader = new THREE.GLTFLoader();
+let ww = innerWidth;
+let wh = innerHeight;
+let renderer = new THREE.WebGLRenderer();
+let camera = new THREE.PerspectiveCamera(100, ww / wh, 0.01, 10);
+let mixer;
+let currentAction;
+const actions = [];
+const field = document.getElementById("buttonField");
+
+resizeHandler();
+camera.position.set(0, 1, 3);
+renderer.gammaOutput = true;
+renderer.gammaFactor = 2.2;
+
+document.body.appendChild(renderer.domElement);
+
+scene.add(new THREE.AmbientLight(0xdddddd));
+
+loader.load("./sample-anim.glb", gltf => {
+  mixer = new THREE.AnimationMixer(gltf.scene);
+  gltf.animations.forEach(animation => {
+    actions.push(mixer.clipAction(animation));
+  });
+  scene.add(gltf.scene);
+
+  actions.forEach(action => {
+    const button = document.createElement("div");
+    button.classList.add("button");
+    button.textContent = action._clip.name;
+    buttonField.appendChild(button);
+
+    button.addEventListener("click", () => {
+      if (currentAction) currentAction.fadeOut(0.5);
+      currentAction = action;
+      currentAction
+        .reset()
+        .play()
+        .fadeIn(0.5);
+      Array.from(buttonField.children).forEach(child =>
+        child.setAttribute("data-active", false)
+      );
+      button.setAttribute("data-active", true);
+    });
+  });
+});
+
+function anim() {
+  requestAnimationFrame(anim);
+  const delta = clock.getDelta();
+  if (!!mixer) mixer.update(delta);
+  renderer.render(scene, camera);
+}
+
+function resizeHandler() {
+  ww = innerWidth;
+  wh = innerHeight;
+  renderer.setSize(ww, wh);
+  camera.aspect = ww / wh;
+  camera.updateProjectionMatrix();
+}
+
+anim();
+window.addEventListener("resize", resizeHandler);
